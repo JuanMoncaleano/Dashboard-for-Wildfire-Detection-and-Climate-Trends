eeaebc91ac0569e19de03805a0d4b3e652d374b6
seanard1 <seanard@gmail.com>
2023-08-19 17:43:17 -0400

    Added groupby to Flask

--- app.py
+++ app.py
@@ -13,11 +13,11 @@ client = MongoClient("mongodb://localhost:27017/")
 db = client["wildfires"]
 
 # Connect to the collections
-collection1 = db["Fire_Point"]
-collection2 = db["Clean_Climate_Data"]
+firePoints = db["Fire_Point"]
+weatherStations = db["Clean_Climate_Data"]
 
 
-# Function to query data based on the year
+# Climate Data by year
 def get_climate_by_specific_year(collection, year):
     try:
         query = {"LOCAL_YEAR": year}
@@ -26,7 +26,62 @@ def get_climate_by_specific_year(collection, year):
     except Exception as e:
         return jsonify({"error": str(e)}), 500
     
-# Function to query data based on the year
+# Climate Data grouped by year
+def get_climate_by_season(collection, start, end):
+    try:
+        pipeline = [
+        {
+            "$match": {
+              "LOCAL_YEAR": {"$gte": start, "$lte": end}
+            }
+        },
+        {
+            "$group": {
+                "_id": "$LOCAL_YEAR",
+                "precip": {"$sum": '$TOTAL_PRECIPITATION'},
+                "mean_temp": {"$avg": "$MEAN_TEMPERATURE"}
+            }
+        },
+        {
+            "$sort": {
+                '_id': 1
+            }
+        }
+        ]
+        data = list(collection.aggregate(pipeline))
+        # data = list(collection.find(query, {"_id": 0}))
+        return jsonify(data)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+# Fire data grouped by year
+def get_fire_by_season(collection, start, end):
+    try:
+        pipeline = [
+        {
+            "$match": {
+                "FIRE_YEAR": {"$gte": start, "$lte": end}
+            }
+        },
+        {
+            "$group": {
+                "_id": "$FIRE_YEAR",
+                "count": {"$sum": 1},
+                "total_burn": {"$sum": "$FIRE_FINAL_SIZE"}
+            }
+        },
+        {
+            "$sort": {
+                '_id': 1
+            }
+        }
+        ]
+        data = list(collection.aggregate(pipeline))
+        return jsonify(data)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+# Fire data grouped by year
 def get_fire_by_specific_year(collection, year):
     try:
         query = {"FIRE_YEAR": year}
@@ -34,6 +89,11 @@ def get_fire_by_specific_year(collection, year):
         return jsonify(data)
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+    
+# # Fire data grouped by year
+# def get_fires_all(collection):
+#     data = collection.find_one()
+#     return print(data)
 
 
 # Define routes with caching
@@ -44,19 +104,37 @@ def welcome():
         f"Available Routes:<br/>"
         f"/api/v1.0/fires/{{year}}<br/>"
         f"/api/v1.0/climate/{{year}}<br/>"
+        f"/api/v1.0/climate/{{start}}/{{end}}<br/>"
+        f"/api/v1.0/fires/{{start}}/{{end}}<br/>"
+        # f"/api/v1.0/fires/all<br/>"
     )
 
 
 @cache.cached(timeout=300)
 @app.route("/api/v1.0/fires/<int:year>")
 def get_fires_by_year(year):
-    return get_fire_by_specific_year(collection1, year)
+    return get_fire_by_specific_year(firePoints, year)
 
 
 @cache.cached(timeout=300)
 @app.route("/api/v1.0/climate/<int:year>")
 def get_climate_by_year(year):
-    return get_climate_by_specific_year(collection2, year)
+    return get_climate_by_specific_year(weatherStations, year)
+
+@cache.cached(timeout=300)
+@app.route("/api/v1.0/climate/<int:start>/<int:end>")
+def get_climate_by_grouped_years(start, end):
+    return get_climate_by_season(weatherStations, start, end)
+
+@cache.cached(timeout=300)
+@app.route("/api/v1.0/climate/<int:start>/<int:end>")
+def get_fires_by_grouped_years(start, end):
+    return get_fire_by_season(firePoints, start, end)
+
+# @cache.cached(timeout=300)
+# @app.route("/api/v1.0/fires/all")
+# def get_fires():
+#     return get_fires_all(firePoints)
 
 
 if __name__ == "__main__":
