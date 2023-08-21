10c203c2615703f7e0f3a3c86a9b255e1819ba0d
seanard1 <seanard@gmail.com>
2023-08-21 17:39:50 -0400

    Fine-tuning layout. Added custom markers and weather data

--- static/js/visualizations.js
+++ static/js/visualizations.js
@@ -3,28 +3,52 @@ let baseUrl = 'http://127.0.0.1:5000/'
 let allFires = 'api/v1.0/fires/1976/2023'
 
 let dropdownURL = baseUrl + allFires
-var markers = [];
+var fireMarkers = [];
+var climateMarkers = [];
+
+
+var dateOptions = { year: 'numeric', month: 'short', day: 'numeric' };
 
 function formatKey (key) {
     return key
 }
 
+// Custom weather station markers declaration
+var weatherMarker = L.ExtraMarkers.icon({
+    icon: 'fa-umbrella', 
+    markerColor: 'green',
+    shape: 'circle',
+    prefix: 'fa'
+  });
+
+// This formats the fire date for popup
+function formatDate (fireDate) {
+    var dateToFix = new Date(fireDate.slice(0, 10));
+    return dateToFix.toLocaleDateString('en-US', dateOptions)
+}
+
+// This formats the fire size for popup
+function formatSize (fireSize) {
+    // reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toLocaleString
+    return fireSize.toLocaleString('en-US', { minimumFractionDigits: 1, maximumFractionDigits: 1 })
+}
 
 
 function changeOutput (year){
 
-    let changeUrl = baseUrl + '/api/v1.0/fires/' + year
+    // API routes for the changing data
+    let changeFireUrl = baseUrl + '/api/v1.0/fires/' + year
+    let changeClimateUrl = baseUrl + '/api/v1.0/climate/' + year
     let yearGroupFireUrl = baseUrl + '/api/v1.0/fires/' + year + '/' + year
     let yearGroupClimateUrl = baseUrl + '/api/v1.0/climate/' + year + '/' + year
 
-
-    // Call for the fire data for the year in question
-    d3.json(changeUrl).then(function(fireYearData) {
+    // Call for the fire points data for the year in question
+    d3.json(changeFireUrl).then(function(fireYearData) {
         
-        console.log(year)
+        // console.log(year)
 
         // Remove the markers from the existing map
-        markers.forEach(function(marker) {
+        fireMarkers.forEach(function(marker) {
             yearlyMap.removeLayer(marker);
         });
 
@@ -34,19 +58,39 @@ function changeOutput (year){
                 fillOpacity: 0.55,
                 fillColor: "red",
                 radius: fireYearData[fire].FIRE_FINAL_SIZE
-            }).bindPopup(`<h1>${fireYearData[fire].FIRE_START_DATE}</h1>`).addTo(yearlyMap);
-            markers.push(marker);
-            
+            }).bindPopup(`<h4>${formatDate(fireYearData[fire].FIRE_START_DATE)}</h4>\
+                <hr>\
+                <h5>Fire size: ${formatSize(fireYearData[fire].FIRE_FINAL_SIZE)} ha </h5>`).addTo(yearlyMap);
+            fireMarkers.push(marker);
         };
 
+        // Call the weather station data for the year from the dropdown
+        d3.json(changeClimateUrl).then(function(climateYearData) {
+
+            // Clear the previous markers
+            climateMarkers.forEach(function(marker2) {
+                yearlyMap.removeLayer(marker2);
+            });
+
+            // console.log(climateYearData)
+
+            // Add the weather stations to the map with custom marker
+            for (let station in climateYearData) {
+                var marker2 = L.marker([climateYearData[station].lat, climateYearData[station].lon], { 
+                    icon: weatherMarker 
+                }).bindPopup(`<h6>${climateYearData[station]._id}</h6> <br /> <h7>${climateYearData[station].precip}`).addTo(yearlyMap);
+                climateMarkers.push(marker2);
+            };
+
+        });
+
+        // Call the grouped fire data for the info panel
         d3.json(yearGroupFireUrl).then(function(fireYearGroupData) {
 
-            // let info = fireYearGroupData
             let infoPanel = d3.select("#year-info").html("");
-            // console.log(info)
             var info = fireYearGroupData[0];
             var keys = Object.keys(info);
-            console.log(keys)
+            // console.log(keys)
             let infoTitle = d3.select(".panel-heading").html("");
             infoTitle.append("h5").text(`Yearly Data for ${info._id}`);
 
@@ -61,13 +105,14 @@ function changeOutput (year){
                 }
             };
 
+            // Call the grouped climate data for the info panel
             d3.json(yearGroupClimateUrl).then(function(climateYearGroupData) {
-                console.log(climateYearGroupData)
+                // console.log(climateYearGroupData)
                 var climateInfo = climateYearGroupData[0];
                 var climateKeys = Object.keys(climateInfo);
 
                 for (key in climateKeys) {
-                    console.log(climateInfo)
+                    // console.log(climateInfo)
                     if (climateKeys[key] != '_id') {
                         infoPanel.append("h6").text(`${formatKey(climateKeys[key])}: ${climateInfo[climateKeys[key]]}`);
                     }
@@ -77,10 +122,12 @@ function changeOutput (year){
     }
 )};
 
+// Looks for the dropdown to change to update viz
 function updateDashboard(year) {
     changeOutput(year);
   };
 
+// Initializes map and dropdown
 function init (){
     
     d3.json(dropdownURL).then(function(data) {
@@ -90,9 +137,9 @@ function init (){
         const yearList = data.map(item => item._id);
 
         for (let n = 0; n < yearList.length; n++) {
-            
             dropDown.append("option").text(yearList[n]).property("value", yearList[n]);
             };
+
         changeOutput(1976); 
     });
 };
